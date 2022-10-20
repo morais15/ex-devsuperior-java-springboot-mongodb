fbfda3522bac3d16a693299b71351f30178f86b0
Douglas Morais <douglasmorais9915@outlook.com>
2022-10-20 11:12:09 -0300

    First commit

---
+++ src/test/java/ex/devsuperior/java/springboot/mongodb/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.devsuperior.java.springboot.mongodb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
